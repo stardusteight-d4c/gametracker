94714a30f4d935ea79e4c9e593262a5b2dec5709
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-08 12:21:51 -0300

    chore: init base layout

---
+++ web/src/app/(home)/page.tsx
@@ -0,0 +1,67 @@
+export default function Home() {
+  return (
+    <main className="bg-dark-str min-h-[100vh] w-screen text-light-str">
+      <div className="flex flex-col max-w-[1022px] w-full mx-auto pt-11">
+        <h1 className="text-3xl font-semibold mb-11">
+          Personalized Stats and Insights Powered by Billions of Matches
+        </h1>
+        <div className="flex mb-8">
+          <div className="flex flex-col">
+            <span className="text-lg font-semibold">15+</span>
+            <span className="text-sm">Games</span>
+          </div>
+          <div className="w-0 h-12 border-l border-l-light-str/20 mx-9" />
+          <div className="flex flex-col">
+            <span className="text-lg font-semibold">500+</span>
+            <span className="text-sm">Hours</span>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-lg font-semibold mb-6">All Games</h3>
+          <div className="flex gap-x-[10px] gap-y-7 flex-wrap">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/b/b0/Persona_5_cover_art.jpg"
+              alt=""
+              className="w-[196px] h-[265px] rounded object-cover border border-dark-low/20 shadow-sm shadow-black/20"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
