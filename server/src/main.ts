1aa1af94ccc02442e14970245d93bc6e81098375
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-16 14:19:12 -0300

    config: server config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,29 @@
-async function bootstrap() {
-  console.log("Hello, World!");
-}
+import { NestFactory } from "@nestjs/core"
+import fastifyCookie, { FastifyCookieOptions } from "@fastify/cookie"
 
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify"
+
+import { AppModule } from "./app.module"
+import { corsOptions } from "@config/cors/corsOptions"
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
+
+  app.register(fastifyCookie, {
+    secret: process.env.FASTIFY_COOKIE_SECRET,
+    parseOptions: {},
+  } as FastifyCookieOptions)
+
+  app.enableCors(corsOptions)
+
+  app.listen(process.env.PORT, "0.0.0.0").then(() => {
+    console.log("🚀 Server running on port:", process.env.PORT)
+  })
+}
 bootstrap()
