ca1b3b4804707208633a11c7e8ff0a648d1595ca
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-19 13:40:33 -0300

    chore: create List game use case

---
+++ server/src/modules/games/app/use-cases/List.ts
@@ -0,0 +1,75 @@
+import { FastifyReply } from "fastify"
+
+import { GameRepository } from "@modules/games/app/decorators/GameRepository"
+
+@GameRepository
+export class List {
+  repository: IGameRepository
+  userId: string
+  params: GameListDTO
+  response: FastifyReply
+
+  public constructor(attr: {
+    params: GameListDTO
+    userId: string
+    response: FastifyReply
+  }) {
+    this.params = attr.params
+    this.userId = attr.userId
+    this.response = attr.response
+  }
+
+  public async execute() {
+    if (this.params.sessionOwner && Boolean(this.params.sessionOwner)) {
+      if (!this.userId) {
+        return this.response.status(401).send({
+          message: "There is no session",
+          error: "Unauthorized",
+          statusCode: 401,
+          data: null,
+        })
+      }
+
+      if (this.userId !== this.params.userId) {
+        return this.response.status(401).send({
+          message: "User session id does not match with games owner",
+          error: "Unauthorized",
+          statusCode: 401,
+          data: null,
+        })
+      }
+
+      const items = await this.repository.list({
+        ...this.params,
+        currentPage: this.params.currentPage
+          ? Number(this.params.currentPage)
+          : undefined,
+        pageSize: this.params.pageSize
+          ? Number(this.params.pageSize)
+          : undefined,
+      })
+
+      return this.response.status(200).send({
+        message: "Games listed successfully",
+        error: null,
+        statusCode: 200,
+        data: items,
+      })
+    }
+
+    const items = await this.repository.list({
+      ...this.params,
+      currentPage: this.params.currentPage
+        ? Number(this.params.currentPage)
+        : undefined,
+      pageSize: this.params.pageSize ? Number(this.params.pageSize) : undefined,
+    })
+
+    return this.response.status(200).send({
+      message: "Games listed successfully",
+      error: null,
+      statusCode: 200,
+      data: items,
+    })
+  }
+}
