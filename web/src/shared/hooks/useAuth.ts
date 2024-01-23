7387326c62ef5f2f83fc712129090a24be4f3100
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-23 12:18:27 -0300

    chore: make getServerSession

--- web/src/shared/hooks/useAuth.ts
+++ web/src/shared/hooks/useAuth.ts
@@ -1,9 +1,26 @@
 import jwt from "jsonwebtoken"
 import { getCookie, deleteCookie } from "../utils"
+import { NextRequest } from "next/server"
 
 export function useAuth() {
   function getUserSession(): DecodedToken | null {
     const accessToken = getCookie("accessToken")
+    if (!accessToken) {
+      console.error("AccessToken cookie not found")
+      return null
+    }
+
+    try {
+      const decodedToken = jwt.decode(accessToken) as DecodedToken
+      return decodedToken
+    } catch (error) {
+      console.error("Error decoding AccessToken:", error)
+      return null
+    }
+  }
+
+  function getServerUserSession(req: NextRequest): DecodedToken | null {
+    const accessToken = req.cookies.get("accessToken")?.value
     if (!accessToken) {
       console.error("AccessToken cookie not found")
       return null
@@ -25,6 +42,7 @@ export function useAuth() {
 
   return {
     getUserSession,
+    getServerUserSession,
     clearAuthCookies,
   }
 }
