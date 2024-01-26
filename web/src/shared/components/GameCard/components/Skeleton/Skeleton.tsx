cbd5bde2217dd1d64af994b284b7413d80cf746b
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-26 11:35:19 -0300

    chore: make GameCard skeleton

---
+++ web/src/shared/components/GameCard/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,25 @@
+export function Skeleton() {
+  return (
+    <div className="lg:w-[196px] col-span-1 rounded flex flex-col pointer-events-none select-none">
+      <div className="lg:w-[196px] h-[245px] lg:h-[265px] relative rounded border border-dark-str/10 overflow-hidden">
+        <div className="w-[196px] h-[265px] flex flex-col items-center justify-center bg-dark-low/10" />
+        <div className="absolute bottom-0 left-0 bg-dark-str/10 px-1 py-2 rounded-tr flex items-center justify-between">
+          <div className="flex flex-col-reverse items-center mt-1 gap-[6px] z-50">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div key={index}>
+                <div className="w-[10px] h-[10px] rounded-full bg-dark-str/20" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <span className="text-sm w-full text-transparent pointer-events-none select-none bg-dark-str/10 rounded-md font-semibold mt-[2px] line-clamp-2">
+        The Legend of Zelda: Tears
+      </span>
+      <span className="text-sm w-fit text-transparent pointer-events-none select-none bg-dark-str/10 rounded-md font-semibold mt-[2px] line-clamp-2">
+        of the Kingdom
+      </span>
+    </div>
+  )
+}
