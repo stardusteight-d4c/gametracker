666ed593a89d7cb3622d4ee124ca5139126a26a5
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-27 11:45:27 -0300

    fix: fixing server side rendering document is not defined

--- web/src/shared/utils/getCookie.ts
+++ web/src/shared/utils/getCookie.ts
@@ -1,13 +1,12 @@
 export function getCookie(name: string): string | null {
-  const cookies = document.cookie.split(';');
-  for (const cookie of cookies) {
-    const [cookieName, cookieValue] = cookie.split('=').map((c) => c.trim());
-    if (cookieName === name) {
-      return decodeURIComponent(cookieValue);
+  if (typeof document !== "undefined") {
+    const cookies = document.cookie.split(";")
+    for (const cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.split("=").map((c) => c.trim())
+      if (cookieName === name) {
+        return cookieValue
+      }
     }
   }
-  return null;
+  return null
 }
-
-
-
